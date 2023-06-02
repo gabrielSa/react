1a66401528a649dc121a4b116a1ffe8421273364
Gabriel Martins <gabrielmmdesa@yahoo.com.br>
2023-06-02 16:59:31 -0300

    v3

--- 6-todo/src/App.jsx
+++ 6-todo/src/App.jsx
@@ -2,35 +2,62 @@ import { useState } from 'react';
 import './App.css';
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
+import Search from './components/Search';
+import Filter from './components/Filter';
 
 
 
 function App() {
   const [todos, setTodos] = useState(todoInitArray);
+  const [search, setSearch] = useState("")
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = ("Asc");
 
   const addTodo = (text, category) => {
-    const newTodos = [
-      ...todos,
-      {
-      id: Math.floor(Math.random*10000),
+    const newTodo =
+    {
+      id: Math.floor(Math.random() * 10000),
       text: text,
       category: category,
       isCompleted: false,
-    },
-  ];
+    }
+
+    const newTodos = [
+      ...todos,
+      newTodo
+    ];
+    console.log(newTodo.id);
+    console.log(typeof newTodo.id)
     setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo => todo.id !== id ? todo : null);
+    setTodos(filteredTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) => todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo);
+    setTodos(newTodos)
   }
 
   return (
     <div className="App">
       <h1>Lista de Tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} />
       <div className='todo-list'>
-        {todos.map((todoElem) => (
-          <Todo key={todoElem.id} todo={todoElem} />
-        ))}
+        {todos
+          .filter((todo) => filter === "All" ? true : filter ==="Completed" ? todo.isCompleted : !todo.isCompleted)
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLocaleLowerCase())).map((todoElem) => (
+              <Todo key={todoElem.id} todo={todoElem} removeTodo={removeTodo} completeTodo={completeTodo} />
+            ))}
       </div>
       <div>
-        <TodoForm addTodo={addTodo}/>
+        <TodoForm addTodo={addTodo} />
       </div>
 
     </div>
--- 6-todo/src/components/Todo.jsx
+++ 6-todo/src/components/Todo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function Todo({todo}) {
+function Todo({todo, removeTodo, completeTodo}) {
 
     return(
-        <div className="todo">
+        <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" :""}}>
 
             <div className="content">
                 <p>{todo.text}</p>
@@ -11,8 +11,8 @@ function Todo({todo}) {
             </div>
 
             <div className = "buttons">
-                <button className="complete">Completar</button>
-                <button className="remove">x</button>
+                <button className="complete" onClick={() => completeTodo(todo.id)}>Completar</button>
+                <button className="remove" onClick={() => removeTodo(todo.id)}>x</button>
             </div>
 
         </div>
