02672f7acc98df1b119398c96df24d7a9676ae25
Gabriel Martins <gabrielmmdesa@yahoo.com.br>
2023-06-12 16:56:02 -0300

    navbar

--- 9-coin/src/App.js
+++ 9-coin/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import {Outlet} from "react-router-dom";
+import Navbar from './layout/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <p>Navbar</p>
+      <Navbar/>
       <h1>React Router</h1>
+      <div className='container min-height'>
       <Outlet/>
+      </div>
       <p>Footer</p>     
     </div>
   );
---
+++ 9-coin/src/layout/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Navbar.css"
+import logo from "../img/coin.png"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="container min-height">
+
+
+
+                <Link to="/">
+                    <img src={logo} alt="Costs" />
+                </Link>
+                <ul className="list">
+                    <li className="item">
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li className="item">
+                        <Link to="contacts">Contato</Link>
+                    </li>
+                    <li className="item">
+                        <Link to="company">Empresa</Link>
+                    </li>
+                    <li className="item">
+                        <Link to="newprojects">Novos Projetos</Link>
+                    </li>
+                </ul>
+
+            </div>
+
+
+        </nav>
+    );
+};
+
+export default Navbar;
